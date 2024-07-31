9db543465ffc5b277d549c224faa10bb0086fcfe
mrabhi1 <guptaabhishek9881@gmail.com>
2024-07-31 15:38:45 +0530

    nhfhyfdhyyfd

--- BASIC OF CPP/percantageOfFiveSubject.cpp
+++ BASIC OF CPP/percantageOfFiveSubject.cpp
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-#include<iostream>
-using namespace std;
-int main()
-{
-    int a,b,c,d,e,total,per;
-
-    cout<<"Enter the marks of 5 SUBJECT:";
-    cin>>a>>b>>c>>d>>e;
-
-    total=a+b+c+d+e;
-
-    cout<<"total marks is"<<total<<endl;
-
-    per=(total*100)/50
-    cout<<"percantage="<<per;
-    sjdbvkjdfvbkjdfvbkjdfvbkjdfbvkjdfvb
-
-    return 0;
-
-=======
 #include<iostream>
 using namespace std;
 int main()
@@ -37,6 +16,3 @@ int main()
     cout<<"percantage="<<per;
 
     return 0;
-
->>>>>>> 936aa74be298bb7c781bc896cf8c80c182d1d61b
-}
