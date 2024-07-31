3870136f883fc56a25c5a62cb2998b0b2a81b203
mrabhi1 <guptaabhishek9881@gmail.com>
2024-07-31 16:40:36 +0530

    hello

---
+++ BASIC OF CPP/areaOfCircle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float area,r;
+
+    cout<<"Entter the radius of circle : ";
+    cin>>r;
+
+    area=3.14*r*r;
+
+    cout<<"Area of circle is "<<area;
+
+    return 0;
+}
