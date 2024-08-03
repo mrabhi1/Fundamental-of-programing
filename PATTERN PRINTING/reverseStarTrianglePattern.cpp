ec9024aca5b79084c76a509ca12cb5ef138bcfc9
mrabhi1 <guptaabhishek9881@gmail.com>
2024-08-03 14:33:25 +0530

    now

---
+++ PATTERN PRINTING/reverseStarTrianglePattern.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 4; i >=2; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"*";
+        }
+
+        cout<<endl;
+        
+    }
+    
+}
---
+++ PATTERN PRINTING/starPlusPattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i < 6; i++)
+    {
+        for (int j=1; j<6; j++)
+        {
+            if (i==3 || j==3)
+            {
+               cout<<"*";
+            }
+            
+            else
+            {
+                cout<<" ";
+            }
+        }
+        
+        cout<<endl;
+        
+    }
+        
+}
