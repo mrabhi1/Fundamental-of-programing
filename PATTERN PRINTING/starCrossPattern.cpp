2bfdbada7dc04591fc670698cd1c252e92cc5b6c
mrabhi1 <guptaabhishek9881@gmail.com>
2024-08-05 18:43:26 +0530

    hiiiiiiii

--- PATTERN PRINTING/starCrossPattern.cpp
+++ PATTERN PRINTING/starCrossPattern.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int main()
 {
-    for (int i = 1; i < 6; i++)
+    for (int i = 1; i <= 5; i++)
     {
-        for (int j=1; j<6; j++)
+        for (int j=1; j<=5; j++)
         {
-            
+            if (j == i || j==(6-i))
+                cout << "*";
+            else
+                cout << " ";
         }
         
         cout<<endl;
